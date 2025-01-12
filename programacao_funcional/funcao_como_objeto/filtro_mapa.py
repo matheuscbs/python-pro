c4a94f7b236de65d94248717074617f0d42dc373
Matheus Cardoso <matheuscbsmsn1994@gmail.com>
2025-01-11 21:21:26 -0300

    study: filter mapa extract versão funcional

--- programacao_funcional/funcao_como_objeto/filtro_mapa.py
+++ programacao_funcional/funcao_como_objeto/filtro_mapa.py
@@ -9,3 +9,11 @@ def possui_nota_maior_que_5(aluno):
 
 
 print(list(filter(possui_nota_maior_que_5, alunos)))
+
+
+def extrair_nome(aluno):
+    nome, _ = aluno
+    return nome
+
+
+print(list(map(extrair_nome, filter(possui_nota_maior_que_5, alunos))))
