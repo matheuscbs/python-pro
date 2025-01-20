52c457f64d33618721b9a107847dd64d58a178f6
Matheus Cardoso <matheuscbsmsn1994@gmail.com>
2025-01-20 15:54:16 -0300

    study: classMethods, staticMethods and Properties

---
+++ oopy/encapsulamento_property.py
@@ -0,0 +1,49 @@
+class Bicicleta:
+    _marca = 'Caloi'
+
+    def __init__(self):
+        self._velocidade = 0
+
+    @classmethod
+    def marca(cls):
+        return cls._marca
+
+    @staticmethod
+    def rodas():
+        return 2
+
+    @property
+    def velocidade(self):
+        print('velocidade')
+        return self._velocidade
+
+    @velocidade.setter
+    def velocidade(self, valor):
+        if valor >= 0:
+            self._velocidade = valor
+        else:
+            self._velocidade = 0
+
+    def pedalar(self):
+        self._velocidade += 10
+
+    def frear(self):
+        self._velocidade -= 3
+
+
+class Monark(Bicicleta):
+    _marca = 'Monark'
+
+
+if __name__ == '__main__':
+    bicicleta = Bicicleta()
+    print(Bicicleta.marca())
+    bicicleta.pedalar()
+    bicicleta.frear()
+    bicicleta.frear()
+    bicicleta.frear()
+    bicicleta.frear()
+    # bicicleta.velocidade = -4  # acessando o endereço de memória e passando um valor não permitido
+    print(bicicleta.velocidade)
+    print(Monark.marca())
+    print(Monark.rodas())
