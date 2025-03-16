4a61a666f928f1b3b31df22a5795293078705181
Matheus Cardoso <matheuscbsmsn1994@gmail.com>
2025-03-16 13:48:16 -0300

    study: composite method with pytest

--- python_patterns/comportamental/strategy/pedido.py
+++ python_patterns/comportamental/strategy/pedido.py
@@ -1,11 +1,19 @@
-from collections import namedtuple
+class Item:
+    def __init__(self, descricao, preco, quantidade):
+        self.preco = preco
+        self.descricao = descricao
+        self.quantidade = quantidade
 
-Item = namedtuple('Item', 'descricao preco quantidade')
+    def total(self):
+        return self.preco * self.quantidade
 
 
 class Pedido:
     def __init__(self):
         self._items = []
 
-    def adicionar(self, item):
-        self._items.append(item)
+    def adicionar(self, *item):
+        self._items.extend(item)
+
+    def subtotal(self):
+        return sum(item.total() for item in self._items)
--- python_patterns/comportamental/strategy/tests/test_pedido.py
+++ python_patterns/comportamental/strategy/tests/test_pedido.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 
+import pytest
+
 from python_patterns.comportamental.strategy.pedido import Item, Pedido
 
 
@@ -8,3 +10,25 @@ def test_adicionar_item():
     pedido = Pedido()
     pedido.adicionar(mac)
     assert 1 == len(pedido._items)
+
+
+@pytest.mark.parametrize(
+    'items,subtotal',
+    [
+        (
+            [Item('Mac', Decimal('9.32'), 2)],
+            '18.64'
+        ),
+        (
+            [
+                Item('Mac', Decimal('9.32'), 2),
+                Item('Galaxy', Decimal('1.02'), 3)
+            ],
+            '21.70'
+        )
+    ]
+)
+def test_subtotal(items, subtotal):
+    pedido = Pedido()
+    pedido.adicionar(*items)
+    assert Decimal(subtotal) == pedido.subtotal()
